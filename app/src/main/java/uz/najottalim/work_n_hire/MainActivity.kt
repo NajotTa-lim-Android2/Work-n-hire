0af962cd74bc7ff5ade7296b473f0c6ad9f653da
mannonov <jahongir.sh.mannonov@gmail.com>
2021-06-17 18:56:45 +0500

    Last commit for "Bobur aka" please fix your bug.

--- app/src/main/java/uz/najottalim/work_n_hire/MainActivity.kt
+++ app/src/main/java/uz/najottalim/work_n_hire/MainActivity.kt
@@ -4,9 +4,11 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.navigation.NavDirections
 import androidx.navigation.fragment.NavHostFragment
 import com.google.firebase.auth.FirebaseAuth
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,6 +28,8 @@ class MainActivity : AppCompatActivity() {
 
         val sharedPreferences =
             getSharedPreferences(getString(R.string.preferences_name), Context.MODE_PRIVATE)
+
+        Log.d("Timber ishlamadi","Shared Preference, ${sharedPreferences.getBoolean(getString(R.string.key_is_hr),false)}")
 
         if (sharedPreferences.getBoolean(getString(R.string.key_is_hr),false)){
             val intent = Intent(this, HumanResourceActivity::class.java)
